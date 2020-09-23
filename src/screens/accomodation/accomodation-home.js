79203185a90134743d36c0dd2def983b976ec40c
athallahannafis <athallahannafis@gmail.com>
2020-09-23 18:23:30 +0700

    Add styling for detail

--- src/screens/accomodation/accomodation-home.js
+++ src/screens/accomodation/accomodation-home.js
@@ -4,6 +4,7 @@ import { Text, View } from 'react-native';
 // style
 import Color from '../../style/color.json';
 import {globalStyling as gs} from '../../style/global-styling';
+import {attractionStyling as ats} from '../../style/attraction-styling';
 
 export default class AccomodationHome extends Component {
   constructor(props) {
