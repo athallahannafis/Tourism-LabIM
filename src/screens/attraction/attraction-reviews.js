ffacfc4ef6ffc105bac247c101712bf216ccb18d
athallahannafis <athallahannafis@gmail.com>
2020-10-02 13:18:09 +0700

    Minor styling edit

--- src/screens/attraction/attraction-reviews.js
+++ src/screens/attraction/attraction-reviews.js
@@ -82,7 +82,7 @@ export default class AttractionReviews extends Component {
       }
       return (
         <View style={[gs.cardSection, {marginBottom: 20}]}>
-          <View style={[gs.rowContainer, {width: '100%'}]}>
+          <View style={[ls.rowContainer, {width: '100%'}]}>
             {/* leftside */}
             <View
               style={{
