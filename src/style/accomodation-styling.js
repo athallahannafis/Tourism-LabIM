5471f0085adb7688864510036df8d1efa762cc84
rasikaayuningtyas <69843457+rasikaayuningtyas@users.noreply.github.com>
2020-10-03 15:42:31 +0700

    Rasikaayuningtyas/26 (#31)
    * fixing container bugs on attraction details
    
    * reservation & payment & sorted reviews
    
    * add slider
    
    * minor fix
    
    * add search results
    
    * filtered by rate success
    
    * filtered by price success
    
    * link accomodation search with reservation
    
    * fix bugs
    
    Co-authored-by: Rasika Ayuningtyas <rasikahayu16@yahoo.com>

--- src/style/accomodation-styling.js
+++ src/style/accomodation-styling.js
@@ -1,6 +1,47 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 import Color from './color.json';
 
-export const attractionStyling = StyleSheet.create({
-
-})
+export const accomodationStyling = StyleSheet.create({
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subCardTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  mainImageinCard: {
+    width: Dimensions.get('window').width - 50,
+    height: 200,
+  },
+  mainImageinCardContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    flex: 1,
+    marginBottom: 5,
+    marginTop: 5,
+  },
+  pilihButton: {
+    width: 90,
+    height: 25,
+    backgroundColor: Color.color6,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  segmentOfCardInSearchResult: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '32%',
+  },
+  smallCardText: {
+    fontSize: 10,
+  },
+  sliderContainer: {
+    marginTop: 10,
+    marginBottom: 5,
+  },
+});
