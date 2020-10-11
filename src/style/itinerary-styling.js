152eccfc9dab647edf904f8c487a941a650f88cf
athallahannafis <athallahannafis@gmail.com>
2020-10-11 10:40:16 +0700

    Add styling file for itinerary

---
+++ src/style/itinerary-styling.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+import Color from './color.json';
+
+export const itineraryStyling = StyleSheet.create({
+  cardInside: {
+    padding: 7,
+    borderWidth: 2,
+    borderColor: Color.color1,
+    borderRadius: 7
+  },
+  cardInsideDetail: {
+    flex: 0,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 7,
+    borderWidth: 2,
+    borderColor: Color.color1,
+    borderRadius: 7
+  },
+  columnContainer: {
+    flex: 0,
+    flexDirection: "column",
+  },
+  cardInsideTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  }
+})
